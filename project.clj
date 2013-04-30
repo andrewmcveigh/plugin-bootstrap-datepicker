c4bf696a2b3d264d45f939461d952fe9bf42c6cf
Andrew Mcveigh <me@andrewmcveigh.com>
2013-04-30 17:14:10 +0200

    Fix project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.andrewmcveigh/plugin-bootstrap "0.1.1-SNAPSHOT"
+(defproject com.andrewmcveigh/plugin-bootstrap-datepicker "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License, Version 2.0"
